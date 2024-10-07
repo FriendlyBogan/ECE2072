578bd751c3d8e906065c1618d5b52b4fb5171f82
Cooper Li <0623cooper@gmail.com>
2024-10-07 14:12:10 +1100

    simp proc no inferred latch

--- ece2072_project_skeleton/proc.v
+++ ece2072_project_skeleton/proc.v
@@ -55,7 +55,7 @@ module simple_proc(clk, rst, din, bus, R0, R1, R2, R3, R4, R5, R6, R7,IR_out,tic
     // TODO: instantiate tick counter:
     tick_FSM tfsm(.clk(clk), .rst(rst), .enable(1'b1), .tick(tick));
 
-    reg [0:0] movi, add, addi, sub, mul, ssi, disp, bez;
+    reg [0:0] movi, add, addi, sub;
 
           
     
@@ -64,36 +64,40 @@ module simple_proc(clk, rst, din, bus, R0, R1, R2, R3, R4, R5, R6, R7,IR_out,tic
         IR_in = 1;
         {A_in, G_in, R0_in, R1_in, R2_in, R3_in, R4_in, R5_in, R6_in, R7_in} = 1'b0;
 		  BUS_control = 0;
+		  IR_in = 0; 
     end 
 
 	
-    always @(tick) begin
+    always @(tick,IR_out) begin
+							{movi, add, addi, sub} = 0;
+                    {A_in, G_in, R0_in, R1_in, R2_in, R3_in, R4_in, R5_in, R6_in, R7_in,IR_in} = 1'b0;
+						  BUS_control = 3'd0;
+						  ALU_op = 3'd0;
+						 
         // TODO: Turn off all control signals:
         // TODO: Turn on specific control signals based on current tick:
         case (tick)
             4'b0001:
                 begin
                     // TODO
-                    {movi, add, addi, sub, mul, ssi, disp, bez} = 0;
-                    {A_in, G_in, R0_in, R1_in, R2_in, R3_in, R4_in, R5_in, R6_in, R7_in} = 1'b0;
                     
-                    IR_in = 0; 
+                   
+                    
                 end
             
             4'b0010:
                 begin
                     case (IR_out[8:6])
-                            3'b000: disp = 1;
                             3'b001: add = 1;
                             3'b010: addi = 1;
                             3'b011: sub = 1;
-                            3'b100: mul = 1;
-                            3'b101: ssi = 1;
-                            3'b110: bez = 1;
                             3'b111: movi = 1;
+									 default: 
+										{add,addi,sub,movi} = 0;
                     endcase
+							
                     // TODO
-                    if (add | sub | addi | mul | ssi) begin
+                    if (add | sub | addi) begin
                         
                         BUS_control = IR_out[5:3]; //Rx
                         
@@ -112,6 +116,7 @@ module simple_proc(clk, rst, din, bus, R0, R1, R2, R3, R4, R5, R6, R7,IR_out,tic
                             3'b101: R5_in = 1;
                             3'b110: R6_in = 1;
                             3'b111: R7_in = 1;
+								
                         endcase
                     end
 				end
@@ -120,14 +125,12 @@ module simple_proc(clk, rst, din, bus, R0, R1, R2, R3, R4, R5, R6, R7,IR_out,tic
                 begin
                     // TODO
                     case (IR_out[8:6])
-                        3'b000: disp = 1;
                         3'b001: add = 1;
                         3'b010: addi = 1;
                         3'b011: sub = 1;
-                        3'b100: mul = 1;
-                        3'b101: ssi = 1;
-                        3'b110: bez = 1;
                         3'b111: movi = 1;
+								default: 
+									{add,addi,sub,movi} = 0;
                     endcase
 
                     if (add) begin
@@ -150,16 +153,7 @@ module simple_proc(clk, rst, din, bus, R0, R1, R2, R3, R4, R5, R6, R7,IR_out,tic
                         
                         ALU_op = 3'b001;
                     
-                    end 
-                    else if (mul) begin
-                        A_in = 0;
-								
-                        BUS_control = IR_out[2:0]; //Ry
-
-                        G_in = 1;
-								
-						ALU_op = 3'b000;
-                    end 
+                    end
 
                     else if (sub) begin
                         A_in = 0;
@@ -168,19 +162,9 @@ module simple_proc(clk, rst, din, bus, R0, R1, R2, R3, R4, R5, R6, R7,IR_out,tic
 
                         G_in = 1;
 								
-						ALU_op = 3'b010;
-                    end 
-                    else if (ssi) begin 
-                        
-                        A_in = 0;
-                        
-                        BUS_control = 4'b1001; //Immediate value
-                        
-                        G_in = 1;
-                        
-                        ALU_op = 3'b011;
+								ALU_op = 3'b010;
                     
-                    end 
+						  end
 
 						  
                     else if (movi) begin
@@ -192,17 +176,15 @@ module simple_proc(clk, rst, din, bus, R0, R1, R2, R3, R4, R5, R6, R7,IR_out,tic
             4'b1000:
                 begin
                     case (IR_out[8:6])
-                        3'b000: disp = 1;
                         3'b001: add = 1;
                         3'b010: addi = 1;
                         3'b011: sub = 1;
-                        3'b100: mul = 1;
-                        3'b101: ssi = 1;
-                        3'b110: bez = 1;
                         3'b111: movi = 1;
+								default: 
+								{add,addi,sub,movi} = 0;
                     endcase
                     // TODO
-                    if (add | sub | addi | mul | ssi) begin
+                    if (add | sub | addi) begin
 
                         BUS_control = 4'b1000;
 
