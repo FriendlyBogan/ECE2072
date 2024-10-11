97fae5369977b21f717c83aaecd214f8cef199de
Cooper Li <0623cooper@gmail.com>
2024-10-11 15:25:36 +1100

    added case for ssi

--- ece2072_project_skeleton/proc_extension_tb.v
+++ ece2072_project_skeleton/proc_extension_tb.v
@@ -6,7 +6,7 @@ This file contains a Verilog test bench to test the correctness of the processor
 Please enter your student ID:
 
 */
-module proc_extension_tb;
+module proc_tb;
     // TODO: Implement the logic of your testbench here
 	 
 	 reg clk, rst;
@@ -14,11 +14,11 @@ module proc_extension_tb;
 	 wire [8:0]IR_out;
 	
 	 reg [8:0] din;
-	 wire[15:0] display;
-	 wire [3:0]tick;
+	 wire [15:0] display
+     wire [3:0]tick;
 	 wire [15:0] bus, R0, R1, R2, R3, R4, R5, R6, R7;
 	 integer error;
-	 proc_extension uut(.clk(clk), .rst(rst), .din(din), .bus(bus), .R0(R0), .R1(R1),.R2(R2), .R3(R3), .R4(R4), .R5(R5), .R6(R6), .R7(R7),.tick(tick), .display(display));
+	 proc_extension uut(.clk(clk), .rst(rst), .din(din), .bus(bus), .R0(R0), .R1(R1),.R2(R2), .R3(R3), .R4(R4), .R5(R5), .R6(R6), .R7(R7),.tick(tick) ,.display(display));
 	 
 	always #5 clk = ~clk;
 	
@@ -129,6 +129,31 @@ module proc_extension_tb;
 	end
 	
 	#33
+
+    	// MOVI FOR SSI
+	din = 9'b111101000; //R7
+	#10
+	din = 9'd30; 
+	#30
+
+	if (R7 != 45) begin 
+		error = error + 1;
+	end 
+	
+	// TEST SSI
+
+	din = 9'b101101000;
+	
+	#10 
+	din = 9'd10;
+	#30 
+	
+	if (R5 != 0) begin 
+		
+		error = error + 1;
+	end 
+	
+	$display("TASK 3 TEST COMPLETE, %d ERRORS",error);
 
 	$finish;
 	end 	
